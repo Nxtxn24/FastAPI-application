9493e84eb97ef20135ba6aad7d10f84b5b98beb2
Nitin <nitinbipinbr@gmail.com>
2026-04-18 12:39:01 +0530

    main.py

--- main.py
+++ main.py
@@ -13,19 +13,5 @@ def root():
     return {"message": "Hello, FastAPI!"}
 
 
-@app.get("/search")
-def search(q: str = ""):
-    return {"query": q}
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    in_stock: bool = True
-
-@app.post("/items/")
-def create_item(item: Item):
-    return {"item": item}
-
 
 Base.metadata.create_all(bind=engine)
