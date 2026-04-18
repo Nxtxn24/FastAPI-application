c5ce5f190767fda73c249945dd469b41cfbe3b0f
Nitin <nitinbipinbr@gmail.com>
2026-04-18 12:14:50 +0530

    Added schemas and CRUD

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from routes import user
+from db.database import Base, engine
 
 app = FastAPI()
 
--- routes/user.py
+++ routes/user.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import SessionLocal
 from models.user import User
+from schemas.user import UserCreate
 
 router = APIRouter()
 
@@ -13,6 +14,46 @@ def get_db():
     finally:
         db.close()
 
+
 @router.get("/")
 def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, age=user.age)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.get("/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+@router.put("/{user_id}")
+def update_user(user_id: int, name: str, age: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    
+    if not user:
+        return {"error": "User not found"}
+    
+    user.name = name
+    user.age = age
+    
+    db.commit()
+    return user
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    
+    if not user:
+        return {"error": "User not found"}
+    
+    db.delete(user)
+    db.commit()
+    
+    return {"message": "User deleted"}
